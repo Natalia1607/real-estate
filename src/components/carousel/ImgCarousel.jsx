b0d859e6f2a1cbcc1412830523da74006a8605f0
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-02-01 13:11:01 +0300

    Slick Carousel

--- src/components/carousel/ImgCarousel.jsx
+++ src/components/carousel/ImgCarousel.jsx
@@ -7,11 +7,43 @@ import slide3 from '../../assets/slide3.jpg';
 import slide4 from '../../assets/slide4.jpg';
 import slide5 from '../../assets/slide5.jpg';
 import slide6 from '../../assets/slide6.jpg';
+import Slider from 'react-slick';
 
 const ImgCarousel = () => {
+    const settings_l = {
+        arrows: false,
+        dots: false,
+        initialSlide: 0,
+        easing:'linear',
+        draggable:true,
+        swipe:true,
+        speed: 9000,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite:true,
+        vertical: true,
+        verticalSwiping: true,
+      };
+
+    const settings_r = {
+        speed: 10000,
+      };
+    
+      /* const generateSlides = ({slides}) => {
+        if(slides){
+             return (
+                <Slider {...settings}>
+                <div>
+                    {slides.map((item)=><div key={item.id}><SlideElement item={item} /></div>
+                    )}
+                    </div> 
+                </Slider>
+            )
+        }
+    } */
   return (
     <div className='carousel'>
-        <div  className='carousel__item carousel__item-l'>
+        <Slider  className='carousel__item carousel__item-l' {...settings_l}>
             <div className='carousel__img'>
                 <img src={slide1} alt="#" />
             </div>
@@ -21,8 +53,8 @@ const ImgCarousel = () => {
             <div className='carousel__img'>
                 <img src={slide3} alt="#" />
             </div>
-        </div>
-        <div className='carousel__item carousel__item-r'>
+        </Slider>
+        <Slider className='carousel__item carousel__item-r' {...settings_l} {...settings_r}>
             <div className='carousel__img'>
                 <img src={slide4} alt="#" />
             </div>
@@ -32,7 +64,7 @@ const ImgCarousel = () => {
             <div className='carousel__img'>
                 <img src={slide6} alt="#" />
             </div>
-        </div>
+        </Slider>
     </div>
   )
 }
